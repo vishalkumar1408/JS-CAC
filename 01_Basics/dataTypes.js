2b2d6d8a42433853999fd5a14d2b88940d50e0e1
vishalkumar1408 <154542157+vishalkumar1408@users.noreply.github.com>
2024-06-16 13:52:28 +0000

    dataTypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // We are using node.js not browser
+
+let name  = "Vishal"
+let age = 18
+let isLoggedIn = true
+
+// string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+    // Symbol => unique
+
+
+// Object
+
+console.log(typeof undefined);
+
+console.log(typeof null);
